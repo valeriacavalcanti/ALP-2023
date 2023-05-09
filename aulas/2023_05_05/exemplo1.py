00a8e1cb1ce67a8f19cbe9fe4887fa66e82be5f8
Valéria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2023-05-09 11:10:57 -0300

    Aula 05/05/2023

---
+++ aulas/2023_05_05/exemplo1.py
@@ -0,0 +1,10 @@
+print('funciona, que legal', end=' IFPB ')
+print('mais coisas')
+print('eita')
+print('que bom')
+
+nome = 'Valéria Maria'
+idade = 15
+
+print(nome, idade)
+print('Nome:', nome, '- idade:', idade, sep='@')
---
+++ aulas/2023_05_05/exemplo2.py
@@ -0,0 +1,12 @@
+numero1 = input("Número 1: ")
+#numero1 = int(numero1)
+numero2 = input("Número 2: ")
+numero3 = input("Número 3: ")
+
+numero1 = int(numero1)
+numero2 = int(numero2)
+numero3 = int(numero3)
+
+media = (numero1 + numero2 + numero3) // 3
+
+print('A média calculada é:', media)
